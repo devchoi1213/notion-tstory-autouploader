f99e2115e49e4a21e7b5a9176f814daa1b9be793
ChoiChanguk <devchoi1213@gmail.com>
2022-10-23 00:42:08 +0900

    feat: Add config system
    Add configuration namespace file and validation file in config folder.
    Register ConfigModule to AppModule.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {ConfigModule} from "@nestjs/config";
+import emailConfig from "./config/email.config";
+import databaseConfig from "./config/database.config";
+import authConfig from "./config/auth.config";
+import {validationSchema} from "./config/config.validation";
+
+const configs = [
+  emailConfig,
+  databaseConfig,
+  authConfig
+]
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      load: [ ...configs],
+      isGlobal: true,
+      validationSchema
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import {Inject, Injectable} from '@nestjs/common';
+import emailConfig from "./config/email.config";
+import {ConfigType} from "@nestjs/config";
 
 @Injectable()
 export class AppService {
+  constructor(
+      @Inject(emailConfig.KEY) private config: ConfigType<typeof emailConfig>
+  ) {}
+
   getHello(): string {
+    console.log(this.config.baseUrl)
     return 'Hello World!';
   }
 }
